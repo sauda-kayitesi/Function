c7d3e7ad34a650e540a7aa2ba8952b7945dae137
Santa Claus <santa@northpole.com>
2018-12-14 10:58:13 +0200

    creat side

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFuction1(){
+    var fisrtSide=document.getElementById("myNumber4").value;
+    alert(fisrtSide);
+}
+
+function myFuction2(){
+    var secondSide=document.getElementById("myNumber6").value;
+    alert(secondSide);
+}
+function myFuction3(){
+    var thirdSide=document.getElementById("myNumber9").value;
+    alert(thirdtSide);
+}
