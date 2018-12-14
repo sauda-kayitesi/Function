60f91d014f891c31108fcc6fed30f7007e781cf2
Santa Claus <santa@northpole.com>
2018-12-14 11:58:44 +0200

    see if is a triangle

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ function myFuction3(){
     var thirdSide=document.getElementById("myNumber9").value;
     alert(thirdtSide);
 }
+
+if(trianhle(4,6,9)){
+    if(is Scalere(4,6,9)){
+        alert("Scalere");
+    }
+}else if(is Equilateral(4,6,9)){
+    alert("equilateral")
+}
