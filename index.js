aa6468a995b5eaefa578879af43b7b3045e05f1d
Santa Claus <santa@northpole.com>
2018-12-14 13:11:33 +0200

    add function

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ function myFuction1(){
     var fisrtSide=document.getElementById("fisrtSide1")   .value;
     var secondSide=document.getElementById("secondSide2") .value;
     var thirdSide=document.getElementById("thirdtSide3")  .value;
+    
 if (fisrtSide==secondSide&&fisrtSide==thirdSide&&secondSide==thirdSide){
 }alert("Equilateral triangle");
 console.log("Equilateral");
+
 else if ("fisrtSide==secondeSide||firstSide==thirdSide||secondSide==thirdSide"){
     alert("Isoceles triangle");
 }
