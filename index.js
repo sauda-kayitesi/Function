3756a91976503de5127145ee9ee8940544923840
Santa Claus <santa@northpole.com>
2018-12-14 14:01:56 +0200

    add identetion

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ function myFuction1(){
     var secondSide=document.getElementById("secondSide2") .value;
     var thirdSide=document.getElementById("thirdtSide3")  .value;
 
-if (fisrtSide===secondSide&&fisrtSide===thirdSide&&secondSide===thirdSide){
-}alert("Equilateral triangle");
-console.log("Equilateral");
+      if (fisrtSide===secondSide&&fisrtSide===thirdSide&&secondSide===thirdSide){
+     }alert("Equilateral triangle");
+     console.log("Equilateral");
 
-else if("fisrtSide==secondeSide||firstSide==thirdSide||secondSide==thirdSide"){
+    else if("fisrtSide==secondeSide||firstSide==thirdSide||secondSide==thirdSide"){
     alert("Isosceles triangle");
     console.log("Isosceles triangle");
-}
-else if(fisrtSide+secondSide>thirdSide&&fisrtSide+thirdSide>secondSide&&secondSide+thirdSide>fisrtSide){
+    }
+    else if(fisrtSide+secondSide>thirdSide&&fisrtSide+thirdSide>secondSide&&secondSide+thirdSide>fisrtSide){
     alert("scalene triangle");
     console.log("Scalene triangle");
-}
-else{
+    }
+    else{
     alert("Not triangle");
-}
+    }
