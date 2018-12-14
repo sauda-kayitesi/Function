f34686e6f453e7981d75c56177a85a2586c1bd8b
Santa Claus <santa@northpole.com>
2018-12-14 12:08:03 +0200

    use else

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ function myFuction2(){
 function myFuction3(){
     var thirdSide=document.getElementById("myNumber9").value;
     alert(thirdtSide);
-}
 
-i
+
+
+}
+if(trianhle(4,6,9)){
+    if(is Scalere(4,6,9)){
+        alert("Scalere");
     }
 }else if(is Equilateral(4,6,9)){
     alert("equilateral")
 }
-f(trianhle(4,6,9)){
-    if(is Scalere(4,6,9)){
-        alert("Scalere");
