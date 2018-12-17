dbdeef76d46355faa96b9233224d5e001dcdc2fc
Santa Claus <santa@northpole.com>
2018-12-17 10:51:30 +0200

    add link in index.html

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-function myFuction1()
-    var Side1=document.getElementById("Side1").value;
-    var Side2=document.getElementById("Side2").value;
-    var Side3=document.getElementById("Side3") .value;
-
-    function myFuction1()
+f
+    function myFuction1(){
      var Side1=parseInter(document.getElementById("Side1").value);
      var Side2=parseInter(document.getElementById("Side2").value);
      var Side3=parseInter(document.getElementById("Side3") .value);
 
       if (Side1===Side2&&Side1===Side3&&Side2===Side3){
-     }alert("Equilateral triangle");
-     console.log("Equilateral");
-    }
-    else if("Side1===Side2||Side1===Side3||Side2===Side3"){
-    alert("Isosceles triangle");
-    console.log("Isosceles triangle");
-    }
+     alert("Equilateral triangle");}
+    
+     else if(Side1===Side2||Side1===Side3||Side2===Side3){
+    alert("Isosceles triangle");}
+    
     else if(fisrtSide+secondSide>thirdSide&&fisrtSide+thirdSide>secondSide&&secondSide+thirdSide>fisrtSide){
-    alert("scalene triangle");
-    console.log("Scalene triangle");
-    }
+    alert("scalene triangle");}
+    
     else{
     alert("Not triangle")
     }
+}
